a53b52ab6c27cac38efb6afe3f148d2a02d84fc9
Jarno02 <jarno.ryhanen02@gmail.com>
2020-11-23 13:53:39 +0200

    saved changes to city search

--- app/src/main/java/com/choicely/mylibrary/CitySearchActivity.java
+++ app/src/main/java/com/choicely/mylibrary/CitySearchActivity.java
@@ -1,20 +1,53 @@
 package com.choicely.mylibrary;
 
 import android.os.Bundle;
-import android.widget.EditText;
+import android.text.Editable;
+import android.widget.AutoCompleteTextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+
 public class CitySearchActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private static final String TAG = "CitySearchActivity";
+    OkHttpClient client = new OkHttpClient();
+
+
+
+//    private String[] fruits = {"Apple", "Appy", "Banana", "Cherry", "Grape", "Kiwi", "Mango", "Pear", "date"};
+
+
+    private AutoCompleteTextView autoCompleteTextView;
+    private Editable textViewInput = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.city_search_activity);
 
-        editText = findViewById(R.id.city_search_activity_edit_text);
+        autoCompleteTextView = findViewById(R.id.city_search_activity_auto_complete_text_view);
+
+        textViewInput = autoCompleteTextView.getText();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://geo-test.choicely.com/search/cities/<city>?limit=10").newBuilder();
+        urlBuilder.addQueryParameter("<city>", String.valueOf(textViewInput));
+        String url = urlBuilder.build().toString();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+//                (this, android.R.layout.select_dialog_item, fruits);
+//        autoCompleteTextView.setThreshold(1); //will start working from first character
+//        autoCompleteTextView.setAdapter(adapter);
+
+
     }
 }
